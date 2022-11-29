c7ad500031d7bcdd25ded72461b477f3d09201e2
yesol <ysoli36512@gmail.com>
2022-11-29 14:39:34 +0900

    스프링 트랜잭션 전파5 - 외부 롤백

--- src/test/java/hello/springtx/propagation/BasicTxTest.java
+++ src/test/java/hello/springtx/propagation/BasicTxTest.java
@@ -1,15 +1,19 @@
 package hello.springtx.propagation;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 import javax.sql.DataSource;
+
 
 @Slf4j
 @SpringBootTest
@@ -20,8 +24,7 @@ public class BasicTxTest {
     @TestConfiguration
     static class Config {
         @Bean
-        public PlatformTransactionManager transactionManager(DataSource
-                                                                     dataSource) {
+        public PlatformTransactionManager transactionManager(DataSource dataSource) {
             return new DataSourceTransactionManager(dataSource);
         }
     }
@@ -45,13 +48,11 @@ public class BasicTxTest {
     @Test
     void double_commit() {
         log.info("트랜잭션1 시작");
-        TransactionStatus tx1 = txManager.getTransaction(new
-                DefaultTransactionAttribute());
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션1 커밋");
         txManager.commit(tx1);
         log.info("트랜잭션2 시작");
-        TransactionStatus tx2 = txManager.getTransaction(new
-                DefaultTransactionAttribute());
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션2 커밋");
         txManager.commit(tx2);
     }
@@ -59,15 +60,47 @@ public class BasicTxTest {
     @Test
     void double_commit_rollback() {
         log.info("트랜잭션1 시작");
-        TransactionStatus tx1 = txManager.getTransaction(new
-                DefaultTransactionAttribute());
+        TransactionStatus tx1 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션1 커밋");
         txManager.commit(tx1);
         log.info("트랜잭션2 시작");
-        TransactionStatus tx2 = txManager.getTransaction(new
-                DefaultTransactionAttribute());
+        TransactionStatus tx2 = txManager.getTransaction(new DefaultTransactionAttribute());
         log.info("트랜잭션2 롤백");
         txManager.rollback(tx2);
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
+
+    @Test
+    void outer_rollback() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new
+                DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new
+                DefaultTransactionAttribute());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+        log.info("외부 트랜잭션 롤백");
+        txManager.rollback(outer);
+    }
+
+
+
+
 
 }
